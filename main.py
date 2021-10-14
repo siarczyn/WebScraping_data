53554496102a415fbc508b9f4052f2960b1cd0e8
siarczyn <57362140+siarczyn@users.noreply.github.com>
2021-10-14 04:00:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,33 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
 
-URL = "https://panoramafirm.pl/salony_i_gabinety_kosmetyczne?sort=1"
-r = requests.get(URL)
+for i in range(1,1838):
+    if (i==1):
+        URL = "https://panoramafirm.pl/salony_i_gabinety_kosmetyczne"
+    else:
+        URL = "https://panoramafirm.pl/salony_i_gabinety_kosmetyczne/firmy,"+str(i)+".html"
+    r = requests.get(URL)
+    print(i)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    matching = soup.find("ul", class_='list-unstyled')
+    new1 = matching.find_all(class_='company-item')
+    df = pd.DataFrame({'nazwa': [], 'strona': [], 'email': [], 'telefon': []})
+    onlyPhones = pd.DataFrame({'nazwa': [], 'strona': [], 'email': [], 'telefon': []})
 
-soup = BeautifulSoup(r.content, 'html.parser')
-matching = soup.find("ul", class_='list-unstyled')
-new1 = matching.find_all(class_='company-item')
-df = pd.DataFrame({'nazwa': [], 'strona': [], 'email': [], 'telefon': []})
+    for elem in new1:
 
-for elem in new1:
-    title = elem.find('a', {'class': 'company-name'})
-    website1 = elem.find('a', {'class': 'icon-website'})
-    website = website1.get('href')
-    email1 = elem.find('a', {'class': 'addax-cs_hl_email_submit_click'})
-    email = email1.get('data-company-email')
-    telefon1 = elem.find('a', {'class': 'addax-cs_hl_phonenumber_click'})
-    telefon = telefon1.get('title')
-    df = df.append({'nazwa': title.text, 'strona': website, 'email': email, 'telefon': telefon}, ignore_index=True)
+        title = elem.find('a', {'class': 'company-name'})
+        website1 = elem.find('a', {'class': 'icon-website'})
+        website = website1.get('href')
+        email1 = elem.find('a', {'class': 'addax-cs_hl_email_submit_click'})
+        email = email1.get('data-company-email')
+        telefon1 = elem.find('a', {'class': 'addax-cs_hl_phonenumber_click'})
+        telefon = telefon1.get('title')
+        if (website != "None"):
+            if (email != "None" or telefon != "None"):
+                df = df.append({'nazwa': title.text, 'strona': website, 'email': email, 'telefon': telefon}, ignore_index=True)
+            if (email == "None" and telefon != "None"):
+                onlyPhones = onlyPhones.append({'nazwa': title.text, 'strona': website, 'email': email, 'telefon': telefon},
+                               ignore_index=True)
 df.to_excel("output.xlsx")
-print(df)
+onlyPhones.to_excel("Only_telephones.xlsx")
